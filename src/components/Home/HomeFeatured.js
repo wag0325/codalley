f425a3e8dc2fe08415ce53725b4c98d426108478
Lydia Kwag <wag0325@gmail.com>
2018-10-07 20:42:29 -0400

    Cleaned up code

--- src/components/Home/HomeFeatured.js
+++ src/components/Home/HomeFeatured.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -14,42 +11,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Hidden from '@material-ui/core/Hidden';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
-import Markdown from '../Utils/Markdown';
 
 const styles = theme => ({
-    layout: {
-        width: 'auto',
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-        [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-            width: 1100,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-    },
-    toolbarMain: {
-        borderBottom: `1px solid ${theme.palette.grey[300]}`,
-    },
-    toolbarTitle: {
-        flex: 1,
-    },
-    toolbarSecondary: {
-        justifyContent: 'space-between',
-    },
-    mainFeaturedPost: {
-        backgroundColor: theme.palette.grey[800],
-        color: theme.palette.common.white,
-        marginBottom: theme.spacing.unit * 4,
-    },
-    mainFeaturedPostContent: {
-        padding: `${theme.spacing.unit * 6}px`,
-        [theme.breakpoints.up('md')]: {
-            paddingRight: 0,
-        },
-    },
-    mainGrid: {
-        marginTop: theme.spacing.unit * 3,
-    },
     card: {
         display: 'flex',
     },
@@ -59,30 +22,7 @@ const styles = theme => ({
     cardMedia: {
         width: 160,
     },
-    markdown: {
-        padding: `${theme.spacing.unit * 3}px 0`,
-    },
-    sidebarAboutBox: {
-        padding: theme.spacing.unit * 2,
-        backgroundColor: theme.palette.grey[200],
-    },
-    sidebarSection: {
-        marginTop: theme.spacing.unit * 3,
-    },
-    footer: {
-        backgroundColor: theme.palette.background.paper,
-        marginTop: theme.spacing.unit * 8,
-        padding: `${theme.spacing.unit * 6}px 0`,
-    },
 });
-
-const sections = [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'Algorithms',
-    'Data Structures',
-];
 
 const featuredPosts = [
     {
@@ -98,23 +38,6 @@ const featuredPosts = [
             'This is a wider card with supporting text below as a natural lead-in to additional content.',
     },
 ];
-
-const archives = [
-    'March 2020',
-    'February 2020',
-    'January 2020',
-    'December 2019',
-    'November 2019',
-    'October 2019',
-    'September 2019',
-    'August 2019',
-    'July 2019',
-    'June 2019',
-    'May 2019',
-    'April 2019',
-];
-
-const social = ['GitHub', 'Twitter', 'Facebook'];
 
 function HomeJumbotron(props) {
     const { classes } = props;
